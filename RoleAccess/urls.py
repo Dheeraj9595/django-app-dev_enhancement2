dddd278f49214b8dc042dc5cc16bb5e3b20d666d
Dheeraj9595 <dheeraj.959595@gmail.com>
2024-05-13 01:07:32 +0530

    first commit

---
+++ RoleAccess/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import AssignGroupView, RemoveUserFromRoleView, toggle_user_status
+
+urlpatterns = [path('assign_group/', AssignGroupView.as_view(), name='assign_group'),
+               path('remove-user-from-role/', RemoveUserFromRoleView.as_view(), name='remove_user_from_role'),
+               path('status/', toggle_user_status, name='toggle_user_status')
+               ]
